348cd57d0ed06110f06a61572a941851a381080e
unknown <m.r.fouladi7@gmail.com>
2023-12-16 18:07:53 +0330

    graphql schema initialize

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
+
+const schema = buildSchema(`
+    type Query {
+        description:  String
+        price: Float
+    }
+`);
+
+const root = {
+  description: "Leather Shoe",
+  price: 220.35,
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+  })
+);
 
 const PORT = 3300;
 
