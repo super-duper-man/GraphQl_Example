6c022aba8d52aa5e52231f6be386544fd20ae50e
unknown <m.r.fouladi7@gmail.com>
2023-12-17 14:10:12 +0330

    add new product's reviews

--- products/products.model.js
+++ products/products.model.js
@@ -35,9 +35,19 @@ function addNewProduct(id, description, price) {
   return newProduct;
 }
 
+function addNewProductReview(id, rating, comment) {
+  const product = getProductById(id);
+  if (product) {
+    const review = { rating, comment };
+    product.reviews.push(review);
+    return review;
+  }
+}
+
 module.exports = {
   getAllProducts,
   getProductByPrice,
   getProductById,
   addNewProduct,
+  addNewProductReview,
 };
